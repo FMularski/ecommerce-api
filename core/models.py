c9231ca140cb9f32e746de6372cfdb1fc35ed022
Filip <filipelblag@gmail.com>
2023-04-27 20:51:22 +0200

    added User.role

--- core/models.py
+++ core/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class CustomUserManager(UserManager):
@@ -28,11 +29,17 @@ class CustomUserManager(UserManager):
         return self._create_user(email, password, **extra_fields)
 
 
+class Role(models.TextChoices):
+    CUSTOMER = ("C", _("Customer"))
+    SELLER = ("S", _("Seller"))
+
+
 class User(AbstractUser):
     username = None
     first_name = None
     last_name = None
     email = models.EmailField(unique=True)
+    role = models.CharField(max_length=1, choices=Role.choices, default=Role.CUSTOMER)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
