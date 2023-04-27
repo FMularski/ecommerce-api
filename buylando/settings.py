2bfa7bf833411139a63cf901c9da12911a722539
Filip <filipelblag@gmail.com>
2023-04-27 18:02:02 +0200

    created core app with a custom User model

--- buylando/settings.py
+++ buylando/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # project apps
+    "core.apps.CoreConfig",
 ]
 
 MIDDLEWARE = [
@@ -131,3 +133,5 @@ STATIC_ROOT = BASE_DIR / "static"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "core.User"
