4a0b0f4b2ad7e06f3ff83c9f5aa4ec409a2fceff
Filip <filipelblag@gmail.com>
2023-04-30 22:29:02 +0200

    implemented PUT,PATCH /products/id/

--- ecommerce/api/views.py
+++ ecommerce/api/views.py
@@ -1,4 +1,3 @@
-# from psycopg2.errors import UndefinedTable
 from django.db.utils import ProgrammingError
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
@@ -12,33 +11,33 @@ from .pagination import ProductPagination
 from .serializers import ProductSerializer, ReadonlyProductSerializer
 
 
+def _get_categories_enum():
+    """
+    Method defining enum of products' categories.
+    It is needed when the enum is needed and the databases is
+    not migrated yet.
+
+    Returns a tuple of lists: names (str) and ids (int)
+    """
+    try:
+        categories = ProductCategory.objects.all()
+        enum_str, enum_int = list(categories.values_list("name", flat=True)), list(
+            categories.values_list("id", flat=True)
+        )
+    except ProgrammingError:
+        enum_str, enum_int = ["Computers", "Smartphones", "Peripherals", "Consoles"], [
+            1,
+            2,
+            3,
+            4,
+        ]
+
+    return enum_str, enum_int
+
+
 class ProductListView(generics.ListCreateAPIView):
     pagination_class = ProductPagination
     parser_classes = [parsers.MultiPartParser]
-
-    @staticmethod
-    def _get_categories_enum():
-        """
-        Method defining enum of products' categories.
-        It is needed when the enum is needed and the databases is
-        not migrated yet.
-
-        Returns a tuple of lists: names (str) and ids (int)
-        """
-        try:
-            categories = ProductCategory.objects.all()
-            enum_str, enum_int = list(categories.values_list("name", flat=True)), list(
-                categories.values_list("id", flat=True)
-            )
-        except ProgrammingError:
-            enum_str, enum_int = ["Computers", "Smartphones", "Peripherals", "Consoles"], [
-                1,
-                2,
-                3,
-                4,
-            ]
-
-        return enum_str, enum_int
 
     @swagger_auto_schema(
         operation_description="Returns the list of products.",
@@ -128,9 +127,9 @@ class ProductListView(generics.ListCreateAPIView):
         return products
 
 
-class ProductDetailView(generics.RetrieveDestroyAPIView):
+class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
-    serializer_class = ReadonlyProductSerializer
+    parser_classes = [parsers.MultiPartParser]
 
     @swagger_auto_schema(
         operation_description="Returns the product with the given id.",
@@ -147,22 +146,62 @@ class ProductDetailView(generics.RetrieveDestroyAPIView):
                 type=openapi.TYPE_STRING,
                 default="Bearer <access>",
             ),
+            openapi.Parameter(
+                "category",
+                openapi.IN_FORM,
+                type=openapi.TYPE_NUMBER,
+                enum=_get_categories_enum()[1],  # int enum
+            ),
+        ],
+    )
+    def put(self, request, *args, **kwargs):
+        return super().put(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_description="Deletes the product with the given id.",
+        manual_parameters=[
+            openapi.Parameter(
+                "Authorization",
+                openapi.IN_HEADER,
+                type=openapi.TYPE_STRING,
+                default="Bearer <access>",
+            ),
+            openapi.Parameter(
+                "category",
+                openapi.IN_FORM,
+                type=openapi.TYPE_NUMBER,
+                enum=_get_categories_enum()[1],  # int enum
+            ),
+        ],
+    )
+    def patch(self, request, *args, **kwargs):
+        return super().patch(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_description="Deletes the product with the given id.",
+        manual_parameters=[
+            openapi.Parameter(
+                "Authorization",
+                openapi.IN_HEADER,
+                type=openapi.TYPE_STRING,
+                default="Bearer <access>",
+            ),
         ],
     )
     def delete(self, request, *args, **kwargs):
         return super().delete(request, *args, **kwargs)
 
     def get_serializer_class(self):
-        if self.request.method in ["DELETE"]:
+        if self.request.method in ["PUT", "PATCH", "DELETE"]:
             return ProductSerializer
         return ReadonlyProductSerializer
 
     def get_authenticators(self):
-        if self.request.method in ["DELETE"]:
+        if self.request.method in ["PUT", "PATCH", "DELETE"]:
             return [auth() for auth in [JWTAuthentication]]
         return []
 
     def get_permissions(self):
-        if self.request.method in ["DELETE"]:
+        if self.request.method in ["PUT", "PATCH", "DELETE"]:
             return [permission() for permission in [permissions.IsAuthenticated, IsSeller]]
         return []
